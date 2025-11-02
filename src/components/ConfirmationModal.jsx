1dd010f23f9a22f82076396ac958756f7f766294
larysalozynska <llozy@softserveinc.com>
2025-11-02 19:00:40 +0000

    Fix sonarqube_v5

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -7,7 +7,6 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm, title, message }) => {
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-      tabIndex={0}
       role="dialog"
       aria-modal="true"
       onClick={onClose}
